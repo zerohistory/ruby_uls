844141dad405d5924c35cec078fc8488732f652d
Pat Nakajima <patnakajima@gmail.com>
2010-07-13 10:04:24 -0400

    No need to be clever, ivar assignment is still cool.

--- lib/uls/client.rb
+++ lib/uls/client.rb
@@ -2,9 +2,15 @@ module ULS
   class Client
     attr_accessor :consumer_key, :consumer_secret,
                   :application_token, :application_token_secret
+    attr_reader :consumer_key, :consumer_secret,
+                :application_token, :application_token_secret
 
     def initialize(credentials={})
-      credentials.each { |key, val| send("#{key}=", val) }
+      @consumer_key = credentials[:consumer_key]
+      @consumer_secret = credentials[:consumer_secret]
+      @application_token = credentials[:application_token]
+      @application_token_secret = credentials[:application_token_secret]
+    end
     end
   end
 end
