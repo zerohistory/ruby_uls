2a35e6238ea762b0f224cb34610a6be3d4d6a24d
Pat Nakajima <patnakajima@gmail.com>
2010-07-13 10:05:17 -0400

    Make these tests more meaningful

--- spec/uls/client_spec.rb
+++ spec/uls/client_spec.rb
@@ -3,14 +3,14 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ULS::Client do
   it "takes credentials" do
     client = ULS::Client.new \
-      :consumer_key => "abcdefg",
-      :consumer_secret => "abcdefg",
-      :application_token => "abcdefg",
-      :application_token_secret => "abcdefg"
+      :consumer_key => "abcdefg1",
+      :consumer_secret => "abcdefg2",
+      :application_token => "abcdefg3",
+      :application_token_secret => "abcdefg4"
 
-    client.consumer_key.should == "abcdefg"
-    client.consumer_secret.should == "abcdefg"
-    client.application_token.should == "abcdefg"
-    client.application_token_secret.should == "abcdefg"
+    client.consumer_key.should == "abcdefg1"
+    client.consumer_secret.should == "abcdefg2"
+    client.application_token.should == "abcdefg3"
+    client.application_token_secret.should == "abcdefg4"
   end
 end
